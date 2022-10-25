46c7b45b32038d8ce7ef417a480e62b9520ddac6
Rahmad Adi Pratama <OhMyVenyx@proton.me>
2022-10-25 14:10:27 +0700

    init bootleg ysl

--- final.sh
+++ final.sh
@@ -8,9 +8,5 @@ export USE_CCACHE=1
 ccache -M 20G -F 0
 ccache -o compression=true
 ccache -z
-
 $lunch
-export ALLOW_MISSING_DEPENDENCIES=true
-export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
-export BUILD_BROKEN_DUP_RULES=true
 $make -j8
